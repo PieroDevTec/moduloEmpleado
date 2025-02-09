e57a0fdd3c2b0b9f7726a5cd85424acaea75d110
mr.webnet <pierodev5@gmail.com>
2025-02-09 11:08:59 -0500

    primera version del modulo acabado

--- README.md
+++ README.md
@@ -1,27 +1,25 @@
-# ModuloEmpleados
+# MODULO DE EMPLEADOS
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.2.7.
+EL MODULO MUESTRA LOS EMPLEADOS QUE ESTAN EN LA BASE DE DATOS DE ORACLE ESQUEMA HR, REALIZA LA BUSQUEDA POR DEPARTAMENTO A LOS QUE PERTENECEN EL EMPLEADO Y TAMBIEN REALIZA LA BUSQUEDA POR EL NOMBRE DEL EMPLEADO
 
-## Development server
+## DEMO
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
+![Busqueda por Departamento](src/assets/demo_departamento.PNG)
 
-## Code scaffolding
+![Busqueda por Departamento](src/assets/demo_empleado.PNG)
 
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
+## INSTALLATION
+El proyecto ha sido generado con  [Angular CLI](https://github.com/angular/angular-cli) version 16.2.7.
+Instalar el proyecto modulo_empleados con npm
 
-## Build
+```bash
+  cd modulo_empleados
+  npm install
+```
+    
+## AUTHORS
 
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory.
+- [@pierodevtec](https://github.com/PieroDevTec/hyperblog)
 
-## Running unit tests
 
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
 
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To use this command, you need to first add a package that implements end-to-end testing capabilities.
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,13 +1,13 @@
-import { Component,OnInit } from '@angular/core';
-import { FormGroup,FormBuilder, Validators } from "@angular/forms";
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 /*SERVICES*/
-import { EmpleadosService  } from "src/app/services/empleados.service";
+import { EmpleadosService } from "src/app/services/empleados.service";
 
 /*INTERFACES*/
-import {Response} from "src/utils/interfaces/response";
+import { Response } from "src/utils/interfaces/response";
 /*UTILS*/
-import { ColDef,GridOptions,GridReadyEvent } from "ag-grid-community";
+import { ColDef, GridOptions, GridReadyEvent } from "ag-grid-community";
 import Swal from "sweetalert2";
 import { NgxSpinnerService } from "ngx-spinner";
 
@@ -16,54 +16,54 @@ import { NgxSpinnerService } from "ngx-spinner";
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit{
+export class HomeComponent implements OnInit {
 
-  form_departamento!:FormGroup;
-  form_empleado!:FormGroup;
+  form_departamento!: FormGroup;
+  form_empleado!: FormGroup;
   categoria = [
-    {id:1,name:'Departamento'},
-    {id:2,name:'Empleado'}
+    { id: 1, name: 'Departamento' },
+    { id: 2, name: 'Empleado' }
   ]
   status = {
-    "form_departamento":true,
-    "form_empleado":false
+    "form_departamento": true,
+    "form_empleado": false
   }
   departamentos = [];
-  columnDefs:ColDef[] = [
-    {field:'codigo',headerName:'Codigo',sortable:true,filter:false},
-    {field:'departmentName',headerName:'Departamento',sortable:true,filter:false},
-    {field:'email',headerName:'Email',sortable:true,filter:false},
-    {field:'firstName',headerName:'Nombre',sortable:true,filter:false},
-    {field:'lastName',headerName:'Apellido',sortable:true,filter:false},
-    {field:'phoneNumber',headerName:'Telefono',sortable:true,filter:false}
+  columnDefs: ColDef[] = [
+    { field: 'codigo', headerName: 'Codigo', sortable: true, filter: false },
+    { field: 'departmentName', headerName: 'Departamento', sortable: true, filter: false },
+    { field: 'email', headerName: 'Email', sortable: true, filter: false },
+    { field: 'firstName', headerName: 'Nombre', sortable: true, filter: false },
+    { field: 'lastName', headerName: 'Apellido', sortable: true, filter: false },
+    { field: 'phoneNumber', headerName: 'Telefono', sortable: true, filter: false }
   ];
-  rowData:any[] = [];
-  gridOptions:GridOptions = {
-    defaultColDef:{
-      filter:true
+  rowData: any[] = [];
+  gridOptions: GridOptions = {
+    defaultColDef: {
+      filter: true
     },
-    paginationPageSizeSelector: [5, 10, 20,100],
-    localeText:this.getSpanishTranslation(),
-    pagination:true,
-    paginationPageSize:5
+    paginationPageSizeSelector: [5, 10, 20, 100],
+    localeText: this.getSpanishTranslation(),
+    pagination: true,
+    paginationPageSize: 5
   }
-  constructor(private fb:FormBuilder,private emplService:EmpleadosService,private spinner:NgxSpinnerService){
+  constructor(private fb: FormBuilder, private emplService: EmpleadosService, private spinner: NgxSpinnerService) {
 
   }
 
-  ngOnInit(){
+  ngOnInit() {
     this.form_departamento = this.fb.group({
-      categoria: ['Departamento',Validators.required],
-      departamento:[null,Validators.required]
+      categoria: ['Departamento', Validators.required],
+      departamento: [null, Validators.required]
     });
     this.form_empleado = this.fb.group({
-      categoria:['Empleado',Validators.required],
-      empleado:[null,Validators.required]
+      categoria: ['Empleado', Validators.required],
+      empleado: [null, Validators.required]
     });
     this.spinner.show()
     this.emplService.getListDepartamento().subscribe(
       (data) => {
-        this.departamentos = data.map( (resp:any) => ({id: resp.departamento, name:resp.departamento}));
+        this.departamentos = data.map((resp: any) => ({ id: resp.departamento, name: resp.departamento }));
         this.spinner.hide();
       },
       (error) => {
@@ -72,126 +72,139 @@ export class HomeComponent implements OnInit{
       }
     )
   }
-  valueFormCategoria(event:any) {
-    if(event.name === 'Empleado'){
+  valueFormCategoria(event: any) {
+    if (event.name === 'Empleado') {
       this.status.form_empleado = true;
       this.status.form_departamento = false;
       this.form_empleado.patchValue({
         categoria: this.categoria.find(cate => cate.name === 'Empleado') || null,
-        empleado:''
+        empleado: ''
       });
       this.spinner.show();
       this.emplService.getListEmpleados().subscribe(
-        (data)=> {
+        (data) => {
           this.rowData = data;
           this.spinner.hide()
         },
-        (error)=> {
+        (error) => {
           this.spinner.hide()
           Swal.fire({
-            icon:"error",
-            title:"Error Server",
-            text:"No se ha podido traer los datos de los servidores",
-            footer:"<p><strong>MODULO DE EMPLEADOS</strong></p>"
+            icon: "error",
+            title: "Error Server",
+            text: "No se ha podido traer los datos de los servidores",
+            footer: "<p><strong>MODULO DE EMPLEADOS</strong></p>"
           });
           console.error(error);
         }
       )
-    }else if(event.name === 'Departamento'){
+    } else if (event.name === 'Departamento') {
       this.status.form_departamento = true;
       this.status.form_empleado = false;
       this.form_departamento.patchValue({
-        categoria:this.categoria.find(cate => cate.name === 'Departamento') || null,
-        departamento:'Accounting'
+        categoria: this.categoria.find(cate => cate.name === 'Departamento') || null,
+        departamento: 'Accounting'
       });
       this.spinner.show();
-      this.emplService.getEmpleByDepartamento({departamento:"Accounting"}).subscribe(
-        (data)=>{
+      this.emplService.getEmpleByDepartamento({ departamento: "Accounting" }).subscribe(
+        (data) => {
           this.rowData = data.list;
           this.spinner.hide();
         },
-        (error)=>{
+        (error) => {
           this.spinner.hide();
           Swal.fire({
-            icon:"error",
-            title:"Error Server",
-            text:"No se ha podido traer los datos de los servidores",
-            footer:"<p><strong>MODULO DE EMPLEADOS</strong></p>"
+            icon: "error",
+            title: "Error Server",
+            text: "No se ha podido traer los datos de los servidores",
+            footer: "<p><strong>MODULO DE EMPLEADOS</strong></p>"
           });
           console.error(error);
         }
       )
     }
   }
-  onSubmitDepartamento(){
+  onSubmitDepartamento() {
     let departamento = this.form_departamento.get('departamento')?.value;
-    let body  = {
+    let body = {
       departamento
     };
     this.spinner.show();
     this.emplService.getEmpleByDepartamento(body).subscribe(
-      (data:any)=>{
-        if(data.codIcon === 0){
+      (data: any) => {
+        if (data.codIcon === 0) {
           this.rowData = data.list;
-        }else {
+        } else {
           Swal.fire({
-            title:'No hay Resultados',
-            text:data.mensaje,
-            icon:data.nameIcon
+            title: 'No hay Resultados',
+            text: data.mensaje,
+            icon: data.nameIcon
           })
         }
         this.spinner.hide();
       },
-      (error)=>{
+      (error) => {
         Swal.fire({
-          title:"Error Server",
-          text:`El servidor se encuentra apago o en un actualizacon.`,
-          icon:"error"
+          title: "Error Server",
+          text: `El servidor se encuentra apago o en un actualizacon.`,
+          icon: "error"
         });
         console.error(error);
         this.spinner.hide();
       }
     )
   };
-  onSubmitEmpleado(){
+  onSubmitEmpleado() {
 
     let data = this.form_empleado.get("empleado")?.value;
-    const empleado = data.split(' ');
 
-    let body = {
-      nombre:empleado[0],
-      apellido:empleado[1]
-    };
-    console.log(body)
-    this.spinner.show();
-    this.emplService.getListEmpleadosData(body).subscribe(
-      (data:Response)=>{
-        console.log(data);
-        this.spinner.hide();
-        if(data.codIcon === 0){
-          this.rowData = data.list;
-        }else if (data.codIcon === 1){
+
+    console.log(data.length);
+
+    if (data.length === 0) {
+      Swal.fire({
+        title: "Observacion",
+        text: "Ingrese el apellido o nombre a buscar",
+        icon: "question",
+        footer: `<p><strong>MODULO DE EMPLEADOS</strong></p>`
+      });
+    } else {
+      const empleado = data.split(' ');
+      let body = {
+        nombre: empleado[0],
+        apellido: empleado[1]
+      };
+      console.log(body)
+      this.spinner.show();
+      this.emplService.getListEmpleadosData(body).subscribe(
+        (data: Response) => {
+          console.log(data);
+          this.spinner.hide();
+          if (data.codIcon === 0) {
+            this.rowData = data.list;
+          } else if (data.codIcon === 1) {
+            Swal.fire({
+              title: "Observacion",
+              text: data.mensaje,
+              icon: "question",
+              footer: `<p><strong>${data.nameApplication}</strong></p>`
+            });
+          }
+        },
+        (error) => {
           Swal.fire({
-            title:"Observacion",
-            text:data.mensaje,
-            icon:"question",
-            footer:`<p><strong>${data.nameApplication}</strong></p>`
+            title: "Error Server",
+            text: `El servidor se encuentra apago o en un actualizacon.`,
+            icon: "error"
           });
+          console.error(error);
+          this.spinner.hide();
         }
-      },
-      (error)=> {
-        Swal.fire({
-          title:"Error Server",
-          text:`El servidor se encuentra apago o en un actualizacon.`,
-          icon:"error"
-        });
-        console.error(error);
-        this.spinner.hide();
-      }
-    )
-    this.form_empleado.patchValue({
-      empleado:''
-    });
+      )
+      this.form_empleado.patchValue({
+        empleado: ''
+      });
+    }
+
 
   }
   //traduccion ag grid
